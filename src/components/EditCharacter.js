fb9580af69e3ac9d461a2c8287e87b25de47051c
Eric Song <ericksong91@gmail.com>
2023-01-29 17:16:33 -0500

    Added edit char component

---
+++ src/components/EditCharacter.js
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+
+
+function EditChar(){
+    //Edit Name with Form element
+    //Edit class with drop down
+    //Use ternary to change the name and class into fields and drop downs with pre filled fields
+
+
+}
+
+export default EditChar
